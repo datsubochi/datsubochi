ebb2a2c7621442693b5ead8351f46349324f7ab3
Rittsu <rrr.ttt.yyy.hand2011@ezweb.ne.jp>
2017-02-26 12:11:25 +0000

    make community

---
+++ app/controllers/mymember_controller.rb
@@ -0,0 +1,45 @@
+class HomeController < ApplicationController
+	before_action:set_note,only:[:show,:edit,:update,:destroy]
+
+  def
+
+  def new
+  	@mymember = Mymember.new
+
+  end
+
+  def create
+  	@mymember = Mymember.new(mymember_params)
+  	@mymember.save
+  	redirect_to @mymember
+
+
+
+  end
+
+  def index
+  	@mymember = Mymember.all
+  end
+
+  def show
+
+  end
+  	
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end	
+
+  private
+  	def set_mymember
+  		@mymember = Mymember.find(params[:id])
+  	end	
+
+    def mymember_params
+    	params.require(:mymember).permit(:title,:body,:image,:place)
+    end	
+end
